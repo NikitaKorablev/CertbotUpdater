0ddf6a30c947fb9cba4d25491c2b5f9f246dfe9f
KorablevNikita <kit06092003@gmail.com>
2025-09-02 10:51:13 +0300

    renew_ssl

---
+++ renew_ssl.py
@@ -0,0 +1,48 @@
+import subprocess
+import time
+import os
+import logging
+from datetime import datetime, timedelta
+
+
+RENEW_INTERVAL_DAYS = 90  # 3 месяца
+
+def init_logger(name: str, log_mod: str = 'a'):
+    name = name.split('.')[-1]
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+
+    # Настройка обработчика и форматировщика для записи в файл
+    file_handler = logging.FileHandler(f"logs/{name}.log", mode=log_mod, encoding='utf-8')
+    formatter = logging.Formatter(
+        fmt="%(asctime)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    # Настройка обработчика для вывода в консоль
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    return logger
+
+
+def renew_certificate():
+    while True:
+        try:
+            # Выполняем команду Certbot для перевыпуска сертификата
+            subprocess.run(["certbot", "renew", "--quiet"], check=True)
+            print(f"Сертификат перевыпущен в {datetime.now()}")
+        except subprocess.CalledProcessError as e:
+            print(f"Ошибка при перевыпуске: {e}")
+        
+        # Ждем 3 месяца
+        time.sleep(RENEW_INTERVAL_DAYS * 24 * 60 * 60)
+
+if __name__ == "__main__":
+    renew_certificate()
